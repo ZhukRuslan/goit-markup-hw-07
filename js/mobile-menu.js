a26171301a7758dfd34fc08559c01838aabea38e
ZhukRuslan <ruslan.zhuk.rz@gmail.com>
2023-04-24 17:06:25 +0300

    add

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,13 +1,42 @@
 (() => {
-    const menuBtnRef = document.querySelector("[data-menu-button]");
-    const mobileMenuRef = document.querySelector("[data-menu]");
-    const mobileBtnClose = document.querySelector("[data-menu-close]");
-  
-    menuBtnRef.addEventListener("click", () => {
-      mobileMenuRef.classList.toggle("is-open");
-    })
+  const mobileMenu = document.querySelector('.js-menu-container');
+  const openMenuBtn = document.querySelector('.js-open-menu');
+  const closeMenuBtn = document.querySelector('.js-close-menu');
+  const toggleMenu = () => {
+    const isMenuOpen =
+      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+    mobileMenu.classList.toggle('is-open');
+    const scrollLockMethod = !isMenuOpen
+      ? 'disableBodyScroll'
+      : 'enableBodyScroll';
+    bodyScrollLock[scrollLockMethod](document.body);
+  };
+  openMenuBtn.addEventListener('click', toggleMenu);
+  closeMenuBtn.addEventListener('click', toggleMenu);
 
-    mobileBtnClose.addEventListener('click', () => {
-      mobileMenuRef.classList.toggle("is-open");
-    });
-  })()
+  // Закрываем мобильное меню на более широких экранах
+  // в случае изменения ориентации устройства.
+  // Close the mobile menu on wider screens if the device orientation changes
+  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+    if (!e.matches) return;
+    mobileMenu.classList.remove('is-open');
+    openMenuBtn.setAttribute('aria-expanded', false);
+    bodyScrollLock.enableBodyScroll(document.body);
+  });
+})();
+
+
+// (() => {
+//     const menuBtnRef = document.querySelector("[data-menu-button]");
+//     const mobileMenuRef = document.querySelector("[data-menu]");
+//     const mobileBtnClose = document.querySelector("[data-menu-close]");
+  
+//     menuBtnRef.addEventListener("click", () => {
+//       mobileMenuRef.classList.toggle("is-open");
+//     })
+
+//     mobileBtnClose.addEventListener('click', () => {
+//       mobileMenuRef.classList.toggle("is-open");
+//     });
+//   })()
